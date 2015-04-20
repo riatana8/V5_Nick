13864843cc9c4565a7093a6bf69aab835df14a50
Lindsay Waldrop <dracoud@gmail.com>
2015-04-20 14:32:15 -0400

    More adds

--- update_target_point_positions.C
+++ update_target_point_positions.C
@@ -89,7 +89,7 @@ void update_target_point_positions(
 	
 	double x;				  //x-Pt specified (rolls over each lag-pt)
 	double w = 0.01;           //Width of Gaussian Wave
-	double A_tilde = 4.8000e+07;   //"Fixed" Amplitude for Gaussian Wave
+	double A_tilde = 4.8000e+01;   //"Fixed" Amplitude for Gaussian Wave
 	double R_o = 0.1;	      //OUTER Radius
 	double R_i = 0.2;        //INNER Radius
 
